f6509783c3d26f986349bfc4a50452fe3982595e
Pyrode <pyrode.01@gmail.com>
2024-08-03 15:58:16 +0530

    Added handleclosecart

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -55,7 +55,9 @@ const Navbar = () => {
     const handleOpenCart = () => {
         setIsCartOpen(false);
     };
-
+    const handleCloseCart = () => {
+        setIsCartOpen(true);
+    };
     // Navigation Menu
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
