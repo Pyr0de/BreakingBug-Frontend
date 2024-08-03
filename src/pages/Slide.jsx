f958ff0a6520df5ddd43542d3bc9f097d37d15b2
VineshRajkumar <vineshrajkumar23@gmail.com>
2024-08-03 14:13:23 +0530

    changed react router to react router dom

--- src/pages/Slide.jsx
+++ src/pages/Slide.jsx
@@ -1,6 +1,5 @@
 import { Divider, Box, Typography, Button, styled, Container } from '@mui/material';
-
-import { Link, useNavigate } from 'react-router';
+import { Link, useNavigate } from 'react-router-dom'; //ERROR FIXED
 import Carousel from 'react-multi-carousel';
 import "react-multi-carousel/lib/styles.css";
 
